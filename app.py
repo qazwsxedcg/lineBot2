bbda45d13109b67b09342fbf4351ad2a12068a5d
qazwsxedcg <51470880+qazwsxedcg@users.noreply.github.com>
2020-02-19 16:55:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def callback():
     user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
     userText = decoded['queryResult']['intent']['displayName']
     #sendText(user,userText)
-    obji=open("studentlist.txt","r")
+    obji=open("https://raw.githubusercontent.com/qazwsxedcg/lineBot2/master/studentlist.txt","r")
  #   sendText(user,"ddd")
 #    for line in obji.readlines():
 #        if number in name:
