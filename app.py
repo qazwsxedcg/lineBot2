2630eab84b0ee9ae9c43575bc1d4a32a742b226e
qazwsxedcg <51470880+qazwsxedcg@users.noreply.github.com>
2020-02-19 17:23:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def index():
     a=os.environ['Authorization']
     obji=open("studentlist.txt","r")
-    b= obji.readline()
+#    b= obji.readline()
     obji.close()
     return "a"
 
