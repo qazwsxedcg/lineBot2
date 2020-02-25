7ee2599d7b4594786b2c82a8c0184069fdcd404c
qazwsxedcg <51470880+qazwsxedcg@users.noreply.github.com>
2020-02-25 17:56:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -37,17 +37,17 @@ def callback():
     userText = decoded['queryResult']['intent']['displayName']
 #    userAction = decoded['queryResult']['parameters']['studentId']
 #    if(userText=="ถามชื่อ"):
-    try:
-            f = open("student.csv", "r")
-            for line in f.readlines():
-                a = line.split(",")
-#              if(userAction==a[0]):
-#                   nameList=nameList+", "+a[4]
-                    sendText(user,a[4])
-            f.close()
-#           sendText(user,nameList)
-    except Exception:
-            sendText(user,"ขออภัย..ไม่สามารถเปิดไฟล์ได้")
+//   #ry:
+    f = open("student.csv", "r")
+    for line in f.readlines():
+        a = line.split(",")
+#       if(userAction==a[0]):
+#            nameList=nameList+", "+a[4]
+                 sendText(user,a[4])
+        f.close()
+#       sendText(user,nameList)
+    #xcept Exception:
+        sendText(user,"ขออภัย..ไม่สามารถเปิดไฟล์ได้")
 #    sendText(user,"ds")
     return '',200
 
