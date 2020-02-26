fa3c5338f1a8cd674720b07028bb222492056493
qazwsxedcg <51470880+qazwsxedcg@users.noreply.github.com>
2020-02-26 09:53:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -51,9 +51,9 @@ def callback():
        try:
             f = open("student.csv", "r")
             for line in f.readlines():
-               a = line.split(",")
-               if(userAction==a[2]):
-                   sendText(user,a[0])
+                a = line.split(",")
+                if(userAction==a[2]):
+                    sendText(user,a[0])
            f.close()
         except Exception:
             sendText(user,"ขออภัย..ไม่สามารถเปิดไฟล์ได้")
