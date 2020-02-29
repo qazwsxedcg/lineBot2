c51582249bb286c3826ec1647661ac1e60c8d93c
qazwsxedcg <51470880+qazwsxedcg@users.noreply.github.com>
2020-02-29 10:15:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -47,17 +47,17 @@ def callback():
             f.close()
         except Exception:
             sendText(user,"ขออภัย..ไม่สามารถเปิดไฟล์ได้")
-    if(userText=="อยากรู้เลขประจำตัว"):
-       try:
-           f = open("student.csv", "r")
-           for line in f.readlines():
-               a = line.split(",")
-               if(userAction2==a[2]):
-                   sendText(user,a[0])
-           f.close()
-        except Exception:
-            sendText(user,"ขออภัย..ไม่สามารถเปิดไฟล์ได้")
-    sendText(user,"ds")
+    #if(userText=="อยากรู้เลขประจำตัว"):
+    #   try:
+    #       f = open("student.csv", "r")
+    #       for line in f.readlines():
+    #           a = line.split(",")
+    #           if(userAction2==a[2]):
+    #               sendText(user,a[0])
+    #       f.close()
+    #    except Exception:
+    #        sendText(user,"ขออภัย..ไม่สามารถเปิดไฟล์ได้")
+    #sendText(user,"ds")
     return '',200
 
 def sendText(user, text):
