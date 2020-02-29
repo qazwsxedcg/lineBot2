b5bdade1e763f8e9952620b3836b09530d896fc4
qazwsxedcg <51470880+qazwsxedcg@users.noreply.github.com>
2020-02-29 12:30:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def callback():
     user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
     userText = decoded['queryResult']['intent']['displayName']
     userAction = decoded['queryResult']['parameters']['studentId']
-    userAction = decoded['queryResult']['parameters']['studentName']
+    userAction2 = decoded['queryResult']['parameters']['studentName']
     if(userText=="ถามชื่อ"):
         try:
             f = open("student.csv", "r")
